714b4a3032913532eba8b072cb47beaa31293daa
Alexander Mykolaichuk <alexmix3000@gmail.com>
2017-07-04 10:39:37 +0300

    Update BreadcrumbsItem.js

--- src/BreadcrumbsItem.js
+++ src/BreadcrumbsItem.js
@@ -5,10 +5,26 @@ import PropTypes from 'prop-types';
 const BreadcrumbsItem = (props) => {
   const { match, name, mappedRoutes } = props;
   const { ActiveLinkComponent, LinkComponent } = props.parentProps;
-
+  
   const findRouteName = url => mappedRoutes[url];
 
-  const routeName = findRouteName(match.url) || name;
+  const matchRouteName = (url, routesCollection) => {
+    let fRouteName = null;
+
+    for (const key in routesCollection) {
+      if (routesCollection.hasOwnProperty(key)) {
+        const routeMatcher = new RegExp(key.replace(/:[^\s/]+/g, '([\\w-]+)'));
+
+        if (url.match(routeMatcher) && key.indexOf(':') !== -1) {
+          fRouteName = routesCollection[key];
+        }
+      }
+    }
+
+    return fRouteName;
+  };
+
+  const routeName = matchRouteName(match.url, mappedRoutes) || (findRouteName(match.url) || name);
 
   if (routeName) {
     return match.isExact
@@ -21,7 +37,6 @@ const BreadcrumbsItem = (props) => {
   }
   return null;
 };
-
 
 
 BreadcrumbsItem.propTypes = {
